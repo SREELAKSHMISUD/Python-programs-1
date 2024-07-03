7da8b09ba51aface45a0cbb5bb9d716bbe8e8735
SREELAKSHMISUD <121602892+SREELAKSHMISUD@users.noreply.github.com>
2024-07-04 01:16:39 +0530

    Add files via upload

---
+++ Printing, input and string functions.py
@@ -0,0 +1,23 @@
+# Day 1 printing
+print("I love python :)")
+
+#print modifiers
+print("HI 'I love' pizza")
+print('HI "I hate" bottleguard')
+print("HI \"I love\" food")
+
+#string concatenation
+print("HEllO" + "" + "WORLD")
+print("I love coding \nCoding is life \nPython is easy")
+
+print("Hello " + "Angel")
+print("Hello" + " " + "Angel")
+print("Hello Angel")
+
+#input function
+input("What is your favorite programming language?")
+print("Hey " + input("What is your name?"))
+
+
+
+
---
+++ input functions.py
@@ -0,0 +1,27 @@
+#input function examples
+num1 = int(input())
+num2 = int(input())
+print(num1 * num2)
+
+# length of strings
+
+print(len("hello"))
+
+# or
+name = len("sree")
+print(name)
+
+# print using variables
+
+name ="sree"
+length= len(name)
+print(length)
+
+#swap numbers using temporary variable
+name = "sree"
+name2= "lakshmi"
+c=name
+name=name2
+name2=c
+print("a:", name)
+print("b:", name2)
